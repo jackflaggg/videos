dd2a0318e9820634cd6b59f061720950790a809e
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2024-02-25 23:11:34 +0500

    add initial app

---
+++ src/my-instruments/start-app/start.ts
@@ -0,0 +1,13 @@
+export const s1 = {}
+
+// 1) инициализируем node приложение:
+//     yarn init --yes
+//
+// 2) устанавливаем express:
+//     yarn add express
+//
+// 3) устанавливаем typescript, nodemon и типы для express, node в devDependencies:
+//     yarn add nodemon typescript ts-node @types/node @types/express --dev
+//
+// 4) генерируем tsconfig
+//     yarn tsc --init
